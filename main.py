95930bde54e25208955b11b3b698b37025b1f4b1
Andy <andydinhdo@gmail.com>
2018-11-02 03:14:53 -0700

    added command line functions auto and manual

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-import pandas as pd
 from extract import store, db_to_csv
-import time
+import pandas as pd
+import time, sys
 
 #make a config.py file and account_lst = [list of accounts]
-from config import account_lst
+
+def app():
+    #auto: runs from list in config.py
+    #manual: user inputs comma separated account list
+    script = sys.argv[0]
+    mode = sys.argv[1]
+    if mode == 'manual':
+        user_input = input('enter accounts comma separated: ')
+        account_lst = user_input.replace(' ','').split(',')
+        print ('You selected: {} \n'.format(account_lst))
+
+    if mode == 'auto':
+        from config import account_lst
 
 
-for account in account_lst:
-    store(account)
-    time.sleep(6)
-    print ('{} extracted! \n'.format(account))
 
-db_to_csv()
+    for account in account_lst:
+        store(account)
+        time.sleep(6)
+        print ('{} extracted! \n'.format(account))
+
+app()
